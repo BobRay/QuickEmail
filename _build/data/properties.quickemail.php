87402bedc2bc23eabfc92d91cf89ae19b416e59d
bobray <bobray@softville.com>
2011-01-07 18:08:22 -0500

    added _build directory

---
+++ _build/data/properties.quickemail.php
@@ -0,0 +1,166 @@
+<?php
+
+
+/**
+ * Properties for QuickEmail
+ *
+ * @package quickemail
+ * @subpackage build
+ */
+
+$properties = array(
+    array(
+        'name' => 'allowHtml',
+        'desc' => 'prop_quickemail.html_desc',
+        'type' => 'combo-boolean',
+        'options' => '',
+        'value' => '1',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'debug',
+        'desc' => 'prop_quickemail.debug_desc',
+        'type' => 'combo-boolean',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'emailSender',
+        'desc' => 'prop_quickemail.emailSender_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'errorHeader',
+        'desc' => 'prop_quickemail.errorHeader_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '<b>Mailer error info: </b>',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'failureMessage',
+        'desc' => 'prop_quickemail.failure_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '<h3 style = "color:red">Send Failed</h3>',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'fromName',
+        'desc' => 'prop_quickemail.fromName_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'hideOutput',
+        'desc' => 'prop_quickemail.hideOutput_desc',
+        'type' => 'combo-boolean',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'message',
+        'desc' => 'prop_quickemail.message_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'msgTpl',
+        'desc' => 'prop_quickemail.msgTpl_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickedit:properties',
+    ),
+    array(
+        'name' => 'replyTo',
+        'desc' => 'prop_quickemail.replyTo_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'smtpErrorHeader',
+        'desc' => 'prop_quickemail.smtpEerrorHeader_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '<h3>Server Debug Information:</h3>',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'subject',
+        'desc' => 'prop_quickemail.subject_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'successMessage',
+        'desc' => 'prop_quickemail.success_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '<h3 style = "color:green">Send reported successful</h3>',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'to',
+        'desc' => 'prop_quickemail.to_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'toName',
+        'desc' => 'prop_quickemail.toName_desc',
+        'type' => 'textfield',
+        'options' => '',
+        'value' => '',
+        'lexicon' => 'quickemail:properties',
+    ),
+    array(
+        'name' => 'DateField',
+        'desc' => '',
+        'type' => 'datefield',
+        'options' => '',
+        'value' => '2010/12/3',
+        'lexicon' => '',
+    ),
+    array(
+        'name' => 'List',
+        'desc' => '',
+        'type' => 'list',
+        'options' => array(
+            array(
+                'name' => 'Option1',
+                'value' => 'Value1',
+                'menu' => '',
+            ),
+            array(
+                'name' => 'Option2',
+                'value' => 'Value2',
+                'menu' => '',
+            ),
+            array(
+                'name' => 'Option3',
+                'value' => 'Value3',
+                'menu' => '',
+            ),
+        ),
+        'value' => 'Value1',
+        'lexicon' => '',
+    ),
+);
+
+return $properties;
---
+++ _build/data/transport.snippets.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package newspublisher
+ * @subpackage build
+ */
+
+function getSnippetContent($filename) {
+    $o = file_get_contents($filename);
+    $o = str_replace('<?php','',$o);
+    $o = str_replace('?>','',$o);
+    $o = trim($o);
+    return $o;
+}
+$snippets = array();
+
+$snippets[0]= $modx->newObject('modSnippet');
+$snippets[0]->fromArray(array(
+    'id' => 0,
+    'name' => 'QuickEmail',
+    'description' => 'Email sending and diagnostic snippet.',
+    'snippet' => getSnippetContent($sources['source_core'].'/snippets/quickemail.snippet.php'),
+),'',true,true);
+$properties = include $sources['data'].'properties.quickemail.php';
+$snippets[0]->setProperties($properties);
+unset($properties);
+
+
+return $snippets;
