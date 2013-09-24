6358f4348d4308391612eb4920a2a9be4369d2a7
bobray <bobray@softville.com>
2013-09-24 16:52:52 -0500

    stop E_NOTICE warnings in build

--- _build/build.config.php
+++ _build/build.config.php
@@ -6,4 +6,11 @@ define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(dirname(__FILE__))))) .
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'assets/');
-?>
+
+/* not used -- here to prevent E_NOTICE warnings */
+if (!defined('MODX_BASE_URL')) {
+    define('MODX_BASE_URL', 'http://localhost/addons/');
+    define('MODX_MANAGER_URL', 'http://localhost/addons/manager/');
+    define('MODX_ASSETS_URL', 'http://localhost/addons/assets/');
+    define('MODX_CONNECTORS_URL', 'http://localhost/addons/connectors/');
+}
