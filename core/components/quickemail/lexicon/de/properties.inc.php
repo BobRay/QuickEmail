f05f7f1a9a70272465ce4cb457e4d1f934572dfe
Sebastian G. Marinescu <sebastian-marinescu@users.noreply.github.com>
2017-07-12 00:13:50 +0200

    Create german translation
    Starting german translation

---
+++ core/components/quickemail/lexicon/de/properties.inc.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * QuickEmail
+ *
+ * Copyright 2010-2017 Bob Ray <https://bobsguides.com>
+ * 6/12/2017
+ *
+ * QuickEmail is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * QuickEmail is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * QuickEmail; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package quickemail
+ * @subpackage lexicon
+ */
+/**
+ * Properties Lexicon Topic
+ *
+ * @package quickemail
+ * @subpackage lexicon
+ * @language de
+ */
+/* QuickEmail properties */
+$_lang['prop_quickemail.message_desc'] = 'Nachricht für den Email-Inhalt; default: `Default Message`.';
+$_lang['prop_quickemail.subject_desc'] = 'Betreff für die Email-Nachricht; default: `Default Subject`.';
+$_lang['prop_quickemail.to_desc'] = 'Address the email message will be sent to; default: emailsender System Setting.';
+$_lang['prop_quickemail.toName_desc'] = 'Value for message toName; default; emailsender System Setting.';
+$_lang['prop_quickemail.fromName_desc'] = 'Value for message fromName; default; site_name System Setting.';
+$_lang['prop_quickemail.emailSender_desc'] = 'Email address for from field of email; default: emailsender System Setting.';
+$_lang['prop_quickemail.replyTo_desc'] = 'Value for replyTo field for email; default: emailsender System Setting.';
+$_lang['prop_quickemail.debug_desc'] = 'Turn on debugging (still attempts to send email); default: no';
+$_lang['prop_quickemail.html_desc'] = 'Allow HTML in message; default: yes';
+$_lang['prop_quickemail.msgTpl_desc'] = 'If sent, the specified chunk will be used for the message body and the &message parameter will be ignored.';
+$_lang['prop_quickemail.hideOutput_desc'] = 'Stifle all output from the snippet; ignored if debug is set; default: No';
+$_lang['prop_quickemail.success_desc'] = 'Mitteilung bei erfolgreicher Nachrichtenzustellung';
+$_lang['prop_quickemail.failure_desc'] = 'Mitteilung bei fehlgeschlagener Nachrichtenzustellung';
+$_lang['prop_quickemail.errorHeader_desc'] = 'Header for mail error message';
+$_lang['prop_quickemail.smtpErrorHeader_desc'] = 'Header for smtp server error messages section';
+
+
+
